05bcda48da2af8a79153cf1080abe34ead45fc0e
larissa felix <larissa.felixx1@gmail.com>
2023-08-06 23:36:46 -0300

    add script para obter informacoes das tabelas

---
+++ projecao.sql
@@ -0,0 +1,9 @@
+/*PROJECAO --> É TUDO QUE VOCE QUER VER NA TELA*/
+
+--select now() as data atual;
+
+--select 5+2 as soma;
+
+--select 2 + 2 as soma, nome, no() from cliente;
+
+--select nome, sexo, tipo, telefone, bairro
